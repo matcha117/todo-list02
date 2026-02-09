ce00376a7673324f73f7eafd313a9a348e47f962
matcha117 <ze.wataru16n@gmail.com>
2026-02-10 02:44:39 +0900

    Add files via upload

---
+++ todo list.js
@@ -0,0 +1,24 @@
+const addButton = document.getElementById('add-button');
+const inputField = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+
+//追加ボタンが押された時の処理
+addButton.addEventListener('click', () => {
+  const taskText = inputField.Value;
+
+  if (taskText !== "") {  //空欄でない場合のみ追加
+    const li =document.createElement('li');
+    li.innerHTML = `
+       <span>${taskText}</span>
+       <button class="delete-btn">削除</button>
+    `;
+
+    //削除ボタンの動作を設定
+    li.querySelector('.delete-btn').addEventListener('click', () => {
+      li.remove();
+    });
+
+    todoList.apoendChild(li);
+    inputField.value = ""; //入力欄を空にする
+  }
+})
